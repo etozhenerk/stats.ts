cff53472911b9f4c52c82e9774fa2a22078c4a65
Mikhail <etozhenerk@yandex.ru>
2022-02-01 23:46:44 +0300

    add new classes

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,8 @@
 import { CsvfileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
 
 const csvFileReader = new CsvfileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 
 matchReader.load();
-
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-
-console.log(`Man United won ${manUnitedWins} times`);
